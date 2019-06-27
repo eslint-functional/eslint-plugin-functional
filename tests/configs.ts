8a53420e82be6e5ce59d887b88a74310a1f2e4a5
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-06-28 09:57:24 +1200

    refactor: Added config types.

--- tests/configs.ts
+++ tests/configs.ts
@@ -1,53 +1,48 @@
-export const typescript = {
+import { Linter } from "eslint";
+
+export const typescript: Linter.Config = {
   parser: require.resolve("@typescript-eslint/parser"),
   parserOptions: {
     sourceType: "module"
   }
 };
 
-export const es10 = {
-  parser: require.resolve("babel-eslint"),
-  parserOptions: {
-    ecmaVersion: 10
-  }
-};
-
-export const es9 = {
+export const es9: Linter.Config = {
   parser: require.resolve("babel-eslint"),
   parserOptions: {
     ecmaVersion: 9
   }
 };
 
-export const es8 = {
+export const es8: Linter.Config = {
   parser: require.resolve("babel-eslint"),
   parserOptions: {
     ecmaVersion: 8
   }
 };
 
-export const es7 = {
+export const es7: Linter.Config = {
   parser: require.resolve("babel-eslint"),
   parserOptions: {
     ecmaVersion: 7
   }
 };
 
-export const es6 = {
+export const es6: Linter.Config = {
   parser: require.resolve("babel-eslint"),
   parserOptions: {
     ecmaVersion: 6
   }
 };
 
-export const es5 = {
+export const es5: Linter.Config = {
   parser: require.resolve("espree"),
   parserOptions: {
     ecmaVersion: 5
   }
 };
 
-export const es3 = {
+export const es3: Linter.Config = {
   parser: require.resolve("espree"),
   parserOptions: {
     ecmaVersion: 3
