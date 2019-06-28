45a9fbfe4dd682a8c325afa503ca1e70910209c6
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-06-28 14:07:34 +1200

    chore: Created configs.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
+import all from "./configs/all.json";
+import base from "./configs/base.json";
+import eslintRecommended from "./configs/eslint-recommended.json";
+import functionalLite from "./configs/functional-lite.json";
+import functional from "./configs/functional.json";
+import recommended from "./configs/recommended.json";
 import { rules } from "./rules";
 
 export default {
-  rules
+  rules,
+  configs: {
+    all,
+    base,
+    recommended,
+    functional,
+    "functional-lite": functionalLite,
+    "eslint-recommended": eslintRecommended
+  }
 };
