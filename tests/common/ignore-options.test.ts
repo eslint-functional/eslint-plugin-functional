d3978190cf122190cd1cd30f6814ef212a673b0f
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2019-07-08 14:18:55 +1200

    test: Add ignore pattern tests.

--- tests/common/ignore-options.test.ts
+++ tests/common/ignore-options.test.ts
@@ -98,8 +98,60 @@ describe("option: ignore", () => {
       }
     ];
 
-    const ruleTester = new RuleTester(typescript);
-    ruleTester.run(
+    new RuleTester(typescript).run(
+      "AssignmentExpression",
+      createDummyRule(context => {
+        const [ignored, options] = context.options;
+        return {
+          AssignmentExpression: node => {
+            expect(shouldIgnore(node, context, options)).toBe(ignored);
+          }
+        };
+      }) as Rule.RuleModule,
+      {
+        valid: tests,
+        invalid: []
+      }
+    );
+  });
+
+  describe("ignorePattern", () => {
+    const tests = [
+      // Prefix match.
+      {
+        code: dedent`
+          mutable_ = 0;
+          mutable_xxx = 0;
+          mutable_xxx.foo = 0;
+          mutable_xxx[0] = 0;`,
+        options: [true, { ignorePattern: "^mutable_" }]
+      },
+      // Suffix match.
+      {
+        code: dedent`
+          _mutable = 0;
+          xxx_mutable = 0;
+          xxx_mutable.foo = 0;
+          xxx_mutable[0] = 0;`,
+        options: [true, { ignorePattern: "_mutable$" }]
+      },
+      // Middle match.
+      {
+        code: dedent`
+          mutable = 0;
+          mutable.foo = 0;
+          mutable[0] = 0;`,
+        options: [true, { ignorePattern: "^mutable$" }]
+      },
+      {
+        code: dedent`
+          mutable.foo.bar = 0;
+          mutable.bar[0] = 0;`,
+        options: [false, { ignorePattern: "^mutable$" }]
+      }
+    ];
+
+    new RuleTester(typescript).run(
       "AssignmentExpression",
       createDummyRule(context => {
         const [ignored, options] = context.options;
