fe6fd7b1c5a68087e12b1c90a6fd340a0cbaef24
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2019-07-15 13:00:46 +1200

    chore(immutable-data): Swap out no-array-mutation and no-object-mutation for immutable-data.

--- src/configs/immutable.ts
+++ src/configs/immutable.ts
@@ -7,14 +7,13 @@ const config = deepMerge([
   {
     rules: {
       "ts-immutable/no-let": "error",
-      "ts-immutable/no-object-mutation": "error",
+      "ts-immutable/immutable-data": "error",
       "ts-immutable/no-delete": "error"
     },
     overrides: [
       {
         files: ["*.ts", "*.tsx"],
         rules: {
-          "ts-immutable/no-array-mutation": "error",
           "ts-immutable/no-method-signature": "warn",
           "ts-immutable/readonly-array": "error",
           "ts-immutable/readonly-keyword": "error"
