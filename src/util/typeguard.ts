15521b67467061dbc60219d359fc3c6e4633f7fc
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2019-07-18 14:52:43 +1200

    refactor(typeguard): Switch to using AST_NODE_TYPES over string literals.

--- src/util/typeguard.ts
+++ src/util/typeguard.ts
@@ -2,7 +2,7 @@
  * This file has functions that typeguard the given node/type.
  */
 
-import { TSESTree, AST_NODE_TYPES } from "@typescript-eslint/typescript-estree";
+import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/typescript-estree";
 // TS import - only use this for types, will be stripped out by rollup.
 import { Type, UnionType } from "typescript";
 // TS import - conditionally imported only when typescript is avaliable.
@@ -34,40 +34,43 @@ export type ObjectConstructorType = Type & {
  * Node type guards.
  */
 
-export function isAssignmentExpression(
+export function isArrayExpression(
   node: TSESTree.Node
 ): node is TSESTree.AssignmentExpression {
-  return node.type === "AssignmentExpression";
+  return node.type === AST_NODE_TYPES.AssignmentExpression;
+}
+
+export function isAssignmentExpression(
+  node: TSESTree.Node
+): node is TSESTree.AssignmentPattern {
+  return node.type === AST_NODE_TYPES.AssignmentPattern;
 }
 
 export function isAssignmentPattern(
   node: TSESTree.Node
-): node is TSESTree.AssignmentPattern {
-  return node.type === "AssignmentPattern";
-}
-
-export function isArrayExpression(
-  node: TSESTree.Node
 ): node is TSESTree.ArrayExpression {
-  return node.type === "ArrayExpression";
+  return node.type === AST_NODE_TYPES.ArrayExpression;
 }
 
 export function isBlockStatement(
   node: TSESTree.Node
 ): node is TSESTree.BlockStatement {
-  return node.type === "BlockStatement";
+  return node.type === AST_NODE_TYPES.BlockStatement;
 }
 
 export function isCallExpression(
   node: TSESTree.Node
 ): node is TSESTree.CallExpression {
-  return node.type === "CallExpression";
+  return node.type === AST_NODE_TYPES.CallExpression;
 }
 
 export function isClassLike(
   node: TSESTree.Node
 ): node is TSESTree.ClassDeclaration | TSESTree.ClassExpression {
-  return node.type === "ClassDeclaration" || node.type === "ClassExpression";
+  return (
+    node.type === AST_NODE_TYPES.ClassDeclaration ||
+    node.type === AST_NODE_TYPES.ClassExpression
+  );
 }
 
 export function isFunctionLike(
@@ -77,53 +80,53 @@ export function isFunctionLike(
   | TSESTree.FunctionExpression
   | TSESTree.ArrowFunctionExpression {
   return (
-    node.type === "FunctionDeclaration" ||
-    node.type === "FunctionExpression" ||
-    node.type === "ArrowFunctionExpression"
+    node.type === AST_NODE_TYPES.FunctionDeclaration ||
+    node.type === AST_NODE_TYPES.FunctionExpression ||
+    node.type === AST_NODE_TYPES.ArrowFunctionExpression
   );
 }
 
 export function isIdentifier(node: TSESTree.Node): node is TSESTree.Identifier {
-  return node.type === "Identifier";
+  return node.type === AST_NODE_TYPES.Identifier;
 }
 
 export function isIfStatement(
   node: TSESTree.Node
 ): node is TSESTree.IfStatement {
-  return node.type === "IfStatement";
+  return node.type === AST_NODE_TYPES.IfStatement;
 }
 
 export function isMemberExpression(
   node: TSESTree.Node
 ): node is TSESTree.MemberExpression {
-  return node.type === "MemberExpression";
+  return node.type === AST_NODE_TYPES.MemberExpression;
 }
 
 export function isMethodDefinition(
   node: TSESTree.Node
 ): node is TSESTree.MethodDefinition {
-  return node.type === "MethodDefinition";
+  return node.type === AST_NODE_TYPES.MethodDefinition;
 }
 
 export function isNewExpression(
   node: TSESTree.Node
 ): node is TSESTree.NewExpression {
-  return node.type === "NewExpression";
+  return node.type === AST_NODE_TYPES.NewExpression;
 }
 
 export function isReturnStatement(
   node: TSESTree.Node
 ): node is TSESTree.ReturnStatement {
-  return node.type === "ReturnStatement";
+  return node.type === AST_NODE_TYPES.ReturnStatement;
+}
+
+export function isTSArrayType(
+  node: TSESTree.Node
+): node is TSESTree.TSIndexSignature {
+  return node.type === AST_NODE_TYPES.TSIndexSignature;
 }
 
 export function isTSIndexSignature(
-  node: TSESTree.Node
-): node is TSESTree.TSIndexSignature {
-  return node.type === "TSIndexSignature";
-}
-
-export function isTSParameterProperty(
   node: TSESTree.Node
 ): node is TSESTree.TSParameterProperty {
   return node.type === AST_NODE_TYPES.TSParameterProperty;
@@ -132,43 +135,43 @@ export function isTSParameterProperty(
 export function isTSInterfaceBody(
   node: TSESTree.Node
 ): node is TSESTree.TSInterfaceBody {
-  return node.type === "TSInterfaceBody";
+  return node.type === AST_NODE_TYPES.TSInterfaceBody;
 }
 
-export function isTSArrayType(
+export function isTSParameterProperty(
   node: TSESTree.Node
 ): node is TSESTree.TSArrayType {
-  return node.type === "TSArrayType";
+  return node.type === AST_NODE_TYPES.TSArrayType;
 }
 
 export function isTSPropertySignature(
   node: TSESTree.Node
 ): node is TSESTree.TSPropertySignature {
-  return node.type === "TSPropertySignature";
+  return node.type === AST_NODE_TYPES.TSPropertySignature;
 }
 
 export function isTSTypeOperator(
   node: TSESTree.Node
 ): node is TSESTree.TSTypeOperator {
-  return node.type === "TSTypeOperator";
+  return node.type === AST_NODE_TYPES.TSTypeOperator;
 }
 
 export function isTypeAliasDeclaration(
   node: TSESTree.Node
 ): node is TSESTree.TSTypeAliasDeclaration {
-  return node.type === "TSTypeAliasDeclaration";
+  return node.type === AST_NODE_TYPES.TSTypeAliasDeclaration;
 }
 
 export function isVariableDeclaration(
   node: TSESTree.Node
 ): node is TSESTree.VariableDeclaration {
-  return node.type === "VariableDeclaration";
+  return node.type === AST_NODE_TYPES.VariableDeclaration;
 }
 
 export function isVariableDeclarator(
   node: TSESTree.Node
 ): node is TSESTree.VariableDeclarator {
-  return node.type === "VariableDeclarator";
+  return node.type === AST_NODE_TYPES.VariableDeclarator;
 }
 
 /*
