86c0ee5d2da37a9d69046b4d6638e4dfcd73874e
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2019-07-19 12:17:13 +1200

    fix(prefer-readonly-types): replace readonly-array and readonly-keyword with prefer-readonly-types

--- tests/util.ts
+++ tests/util.ts
@@ -61,7 +61,7 @@ export function processInvalidTestCase(
       ];
     },
     []
-    /* eslint-disable ts-immutable/readonly-array */
+    /* eslint-disable-next-line ts-immutable/prefer-readonly-types */
   ) as Array<ESLintRuleTester.InvalidTestCase>;
 }
 
@@ -89,5 +89,5 @@ export function createDummyRule(
   };
 
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  return createRule<"generic", Array<any>>("dummy", meta, [], create);
+  return createRule<"generic", ReadonlyArray<any>>("dummy", meta, [], create);
 }
