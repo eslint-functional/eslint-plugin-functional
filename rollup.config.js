253e5f6d09780b4678035bdb608aaa0fd535e73a
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-10-08 07:41:39 +1300

    chore: better local file detection

--- rollup.config.js
+++ rollup.config.js
@@ -8,13 +8,28 @@ import rollupPluginNodeResolve from "rollup-plugin-node-resolve";
 import rollupPluginTypescript from "rollup-plugin-typescript2";
 import rollupPluginJSON from "rollup-plugin-json";
 
+import {
+  isAbsolute as isAbsolutePath,
+  join as joinPaths,
+  normalize as normalizePath
+} from "path";
+
 const common = {
   input: "src/index.ts",
 
   external: id => {
-    return (
-      // Not a Local File?
-      !(id.startsWith(".") || id.startsWith("/"))
+    const localPaths = [".", process.cwd()];
+    const excludedPaths = ["node_modules"];
+    const normalId = isAbsolutePath(id) ? normalizePath(id) : id;
+
+    return !localPaths.some(
+      localPath =>
+        // Local file?
+        normalId.startsWith(localPath) &&
+        // Not excluded?
+        !excludedPaths.some(excludePath =>
+          normalId.startsWith(joinPaths(localPath, excludePath))
+        )
     );
   },
 
