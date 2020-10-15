ad33e8bf06507a74d48e5c5f378554050d557a98
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2020-10-15 15:36:41 +1300

    feat(prefer-tacit): new rule: prefer-tacit
    rule set up with ability to handle the case of a function wrapper with x parameters calling a
    function passing all x parameters in the same order that is typed as takeing x parameters.

---
+++ src/configs/stylitic.ts
@@ -0,0 +1,17 @@
+import { Linter } from "eslint";
+
+const config: Linter.Config = {
+  rules: {
+    "functional/prefer-tacit": ["warn", { assumeTypes: { allowFixer: false } }],
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "functional/prefer-tacit": ["error", { assumeTypes: false }],
+      },
+    },
+  ],
+};
+
+export default config;
