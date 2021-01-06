f12d96e6acfdd01174ad11894e492d8e48601f49
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-01-07 02:34:46 +1300

    fix: bring rule meta data "recommended" into line with the recommend config

--- src/rules/no-expression-statement.ts
+++ src/rules/no-expression-statement.ts
@@ -49,7 +49,7 @@ const meta: RuleMetaData<keyof typeof errorMessages> = {
   docs: {
     description: "Disallow expression statements.",
     category: "Best Practices",
-    recommended: false,
+    recommended: "error",
   },
   messages: errorMessages,
   schema,
