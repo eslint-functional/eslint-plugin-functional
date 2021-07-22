967c0fd784c5e772c8deca38730ff29059074e5a
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-07-23 03:50:36 +1200

    style: update style for latest prettier

--- tests/helpers/util.ts
+++ tests/helpers/util.ts
@@ -75,10 +75,10 @@ export function createDummyRule(
     schema: {},
   };
 
-  return ({
+  return {
     meta,
     create,
-  } as unknown) as Rule.RuleModule;
+  } as unknown as Rule.RuleModule;
 }
 
 export type RuleTesterTests = {
