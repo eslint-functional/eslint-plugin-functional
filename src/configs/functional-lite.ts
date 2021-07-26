ce741a38b43e2d082fb027057c56211d6271f2c3
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-07-27 04:38:00 +1200

    feat(functional-lite): adjust immutable-data options for lite config

--- src/configs/functional-lite.ts
+++ src/configs/functional-lite.ts
@@ -5,6 +5,7 @@ import functional from "./functional";
 
 const config: Linter.Config = deepMerge(functional, {
   rules: {
+    "functional/immutable-data": ["error", { ignoreClass: "fieldsOnly" }],
     "functional/no-conditional-statement": "off",
     "functional/no-expression-statement": "off",
     "functional/no-try-statement": "off",
