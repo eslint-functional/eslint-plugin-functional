4c741e13ae2bf63fbe71042515ed2b84b5ca253e
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-10-15 12:57:49 +1300

    feat(prefer-tacit): disable prefer-tacit by default in recommended and lite configs

--- src/configs/functional.ts
+++ src/configs/functional.ts
@@ -8,13 +8,28 @@ import noObjectOrientation from "~/configs/no-object-orientation";
 import noStatements from "~/configs/no-statements";
 import stylistic from "~/configs/stylistic";
 
+const overrides: Linter.Config = {
+  rules: {
+    "functional/prefer-tacit": "off",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "functional/prefer-tacit": "off",
+      },
+    },
+  ],
+};
+
 const config: Linter.Config = deepmerge(
   currying,
   noMutations,
   noExceptions,
   noObjectOrientation,
   noStatements,
-  stylistic
+  stylistic,
+  overrides
 );
 
 export default config;
