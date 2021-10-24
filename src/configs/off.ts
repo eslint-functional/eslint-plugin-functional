5ad533d4d750624fa5791fd9d8a53292e3991846
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-10-25 01:51:47 +1300

    fix: off config no longer contains "overrides" (#286)

--- src/configs/off.ts
+++ src/configs/off.ts
@@ -2,20 +2,20 @@ import type { Linter } from "eslint";
 
 import all from "./all";
 
-function turnRulesOff(rules: Linter.Config["rules"]): Linter.Config["rules"] {
+function turnRulesOff(rules: ReadonlyArray<string>): Linter.Config["rules"] {
   return rules === undefined
     ? undefined
-    : Object.fromEntries(
-        Object.entries(rules).map(([name, value]) => [name, "off"])
-      );
+    : Object.fromEntries(rules.map((name) => [name, "off"]));
 }
 
+const allRulesNames = new Set([
+  ...Object.keys(all.rules ?? {}),
+  ...(all.overrides?.flatMap((override) => Object.keys(override.rules ?? {})) ??
+    []),
+]);
+
 const config: Linter.Config = {
-  rules: turnRulesOff(all.rules),
-  overrides: all.overrides?.map((override) => ({
-    ...override,
-    rules: turnRulesOff(override.rules),
-  })),
+  rules: turnRulesOff([...allRulesNames]),
 };
 
 export default config;
