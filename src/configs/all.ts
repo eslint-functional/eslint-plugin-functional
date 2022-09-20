dc60a00bf6cc52ca40e97475926b5e5e3c8cb199
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-09-21 01:58:17 +1200

    feat!: update ruleset configurations

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -1,33 +1,46 @@
 import type { Linter } from "eslint";
 
+import * as functionalParameters from "~/rules/functional-parameters";
+import * as immutableData from "~/rules/immutable-data";
+import * as noClass from "~/rules/no-class";
+import * as noConditionalStatement from "~/rules/no-conditional-statement";
+import * as noExpressionStatement from "~/rules/no-expression-statement";
+import * as noLet from "~/rules/no-let";
+import * as noLoop from "~/rules/no-loop-statement";
+import * as noMixedType from "~/rules/no-mixed-type";
+import * as noPromiseReject from "~/rules/no-promise-reject";
+import * as noReturnVoid from "~/rules/no-return-void";
+import * as noThisExpression from "~/rules/no-this-expression";
+import * as noThrowStatement from "~/rules/no-throw-statement";
+import * as noTryStatement from "~/rules/no-try-statement";
+import * as preferImmutableParameterTypes from "~/rules/prefer-immutable-parameter-types";
+import * as preferPropertySignatures from "~/rules/prefer-property-signatures";
+import * as preferTacit from "~/rules/prefer-tacit";
+import * as typeDeclarationImmutability from "~/rules/type-declaration-immutability";
+
 const config: Linter.Config = {
   rules: {
-    "functional/functional-parameters": "error",
-    "functional/immutable-data": "error",
-    "functional/no-class": "error",
-    "functional/no-conditional-statement": "error",
-    "functional/no-expression-statement": "error",
-    "functional/no-let": "error",
-    "functional/no-loop-statement": "error",
-    "functional/no-promise-reject": "error",
-    "functional/no-this-expression": "error",
-    "functional/no-throw-statement": "error",
-    "functional/no-try-statement": "error",
-    "functional/prefer-tacit": ["warn", { assumeTypes: { allowFixer: false } }],
+    [`functional/${functionalParameters.name}`]: "error",
+    [`functional/${immutableData.name}`]: "error",
+    [`functional/${noClass.name}`]: "error",
+    [`functional/${noConditionalStatement.name}`]: "error",
+    [`functional/${noExpressionStatement.name}`]: "error",
+    [`functional/${noLet.name}`]: "error",
+    [`functional/${noLoop.name}`]: "error",
+    [`functional/${noMixedType.name}`]: "error",
+    [`functional/${noPromiseReject.name}`]: "error",
+    [`functional/${noReturnVoid.name}`]: "error",
+    [`functional/${noThisExpression.name}`]: "error",
+    [`functional/${noThrowStatement.name}`]: "error",
+    [`functional/${noTryStatement.name}`]: "error",
+    [`functional/${preferImmutableParameterTypes.name}`]: "error",
+    [`functional/${preferPropertySignatures.name}`]: "error",
+    [`functional/${preferTacit.name}`]: [
+      "warn",
+      { assumeTypes: { allowFixer: false } },
+    ],
+    [`functional/${typeDeclarationImmutability.name}`]: "error",
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      rules: {
-        "functional/no-mixed-type": "error",
-        "functional/prefer-immutable-parameter-types": "error",
-        "functional/prefer-property-signatures": "error",
-        "functional/prefer-tacit": ["error", { assumeTypes: false }],
-        "functional/no-return-void": "error",
-        "functional/type-declaration-immutability": "error",
-      },
-    },
-  ],
 };
 
 export default config;
--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -6,30 +6,13 @@ import noExceptions from "~/configs/no-exceptions";
 import noMutations from "~/configs/no-mutations";
 import noObjectOrientation from "~/configs/no-object-orientation";
 import noStatements from "~/configs/no-statements";
-import stylistic from "~/configs/stylistic";
-
-const overrides: Linter.Config = {
-  rules: {
-    "functional/prefer-tacit": "off",
-  },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      rules: {
-        "functional/prefer-tacit": "off",
-      },
-    },
-  ],
-};
 
 const config: Linter.Config = deepmerge(
   currying,
   noMutations,
   noExceptions,
   noObjectOrientation,
-  noStatements,
-  stylistic,
-  overrides
+  noStatements
 );
 
 export default config;
