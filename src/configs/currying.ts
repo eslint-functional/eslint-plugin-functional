6f943c8accb2021f4db5c4268c29bf3e2cf46f6a
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-09-21 01:58:17 +1200

    feat!: update ruleset configurations

--- src/configs/currying.ts
+++ src/configs/currying.ts
@@ -1,8 +1,10 @@
 import type { Linter } from "eslint";
 
+import * as functionalParameters from "~/rules/functional-parameters";
+
 const config: Linter.Config = {
   rules: {
-    "functional/functional-parameters": "error",
+    [`functional/${functionalParameters.name}`]: "error",
   },
 };
 
