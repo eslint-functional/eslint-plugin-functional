6a4b366e6ea6b45217eaafbb98847b9443ceff9e
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-09-22 11:41:38 +1200

    refactor: rename ruleset files

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import all from "~/configs/all";
 import currying from "~/configs/currying";
 import externalTypeScriptRecommended from "~/configs/external-typescript-recommended";
 import externalVanillaRecommended from "~/configs/external-vanilla-recommended";
-import functional from "~/configs/functional";
-import functionalLite from "~/configs/functional-lite";
 import noExceptions from "~/configs/no-exceptions";
 import noMutations from "~/configs/no-mutations";
 import noObjectOrientation from "~/configs/no-object-orientation";
@@ -13,6 +11,8 @@ import noStatements from "~/configs/no-statements";
 import off from "~/configs/off";
 import stylistic from "~/configs/stylistic";
 import { rules } from "~/rules";
+import lite from "~/src/configs/lite";
+import recommended from "~/src/configs/recommended";
 
 /**
  * The config type object ESLint is expecting.
@@ -26,16 +26,16 @@ const config: EslintPluginConfig = {
   rules,
   configs: {
     all,
-    recommended: functional,
+    lite,
+    recommended,
+    off,
     "external-vanilla-recommended": externalVanillaRecommended,
     "external-typescript-recommended": externalTypeScriptRecommended,
-    lite: functionalLite,
-    off,
-    "no-mutations": noMutations,
+    currying,
     "no-exceptions": noExceptions,
+    "no-mutations": noMutations,
     "no-object-orientation": noObjectOrientation,
     "no-statements": noStatements,
-    currying,
     stylistic,
   },
 };
