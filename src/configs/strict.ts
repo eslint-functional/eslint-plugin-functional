ab491924571990e571925d413a08efbe67486801
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-09-25 00:56:50 +1200

    feat!: rename ruleset `no-object-orientation` to `no-other-paradigms`

--- src/configs/strict.ts
+++ src/configs/strict.ts
@@ -4,14 +4,14 @@ import type { Linter } from "eslint";
 import currying from "~/configs/currying";
 import noExceptions from "~/configs/no-exceptions";
 import noMutations from "~/configs/no-mutations";
-import noObjectOrientation from "~/configs/no-object-orientation";
 import noStatements from "~/configs/no-statements";
+import noOtherParadigms from "~/src/configs/no-other-paradigms";
 
 const config: Linter.Config = deepmerge(
   currying,
   noMutations,
   noExceptions,
-  noObjectOrientation,
+  noOtherParadigms,
   noStatements
 );
 
