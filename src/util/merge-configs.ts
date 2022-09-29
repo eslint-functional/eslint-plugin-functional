0bca5f7fca2824ccb3c91efc1e5f052d2b107138
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-09-29 16:38:13 +1300

    refactor: use custom merge function to merge configs

---
+++ src/util/merge-configs.ts
@@ -0,0 +1,35 @@
+import { deepmergeCustom } from "deepmerge-ts";
+
+export const mergeConfigs = deepmergeCustom<
+  {},
+  {
+    keyPath: ReadonlyArray<PropertyKey>;
+  }
+>({
+  metaDataUpdater: (previousMeta, metaMeta) => {
+    if (previousMeta === undefined) {
+      if (metaMeta.key === undefined) {
+        return { keyPath: [] };
+      }
+      return { keyPath: [metaMeta.key] };
+    }
+    if (metaMeta.key === undefined) {
+      return previousMeta;
+    }
+    return {
+      ...metaMeta,
+      keyPath: [...previousMeta.keyPath, metaMeta.key],
+    };
+  },
+  mergeArrays(values, utils, meta) {
+    if (
+      meta !== undefined &&
+      meta.keyPath.length >= 2 &&
+      meta.keyPath[0] === "rules"
+    ) {
+      return utils.actions.skip;
+    }
+
+    return utils.actions.defaultMerge;
+  },
+});
