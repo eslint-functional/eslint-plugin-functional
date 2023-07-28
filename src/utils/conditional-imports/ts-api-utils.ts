4574faf0d76cac2b2bbdab62bced98092494aeb9
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-07-28 23:03:48 +1200

    docs: remove some comments

--- src/utils/conditional-imports/ts-api-utils.ts
+++ src/utils/conditional-imports/ts-api-utils.ts
@@ -2,7 +2,6 @@ import type tsApiUtils from "ts-api-utils";
 
 import ts from "#eslint-plugin-functional/conditional-imports/typescript";
 
-// Conditionally loaded ts-api-utils but only if TypeScript is available.
 export default (() => {
   if (ts !== undefined) {
     return require("ts-api-utils") as typeof tsApiUtils;
--- src/utils/conditional-imports/typescript.ts
+++ src/utils/conditional-imports/typescript.ts
@@ -1,7 +1,5 @@
-// Note: This import will be stripped out by rollup.
 import type ts from "typescript";
 
-// Conditionally loaded TypeScript but only if it is available.
 export default (() => {
   try {
     return require("typescript") as typeof ts;
