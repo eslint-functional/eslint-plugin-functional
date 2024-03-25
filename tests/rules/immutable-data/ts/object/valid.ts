059591af0507162092a36630b57619686b513f70
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2024-03-25 23:05:26 +1300

    feat(immutable-data): add option for `ignoreNonConstDeclarations` to `treatParametersAsConst` (#794)
    fix #724

--- tests/rules/immutable-data/ts/object/valid.ts
+++ tests/rules/immutable-data/ts/object/valid.ts
@@ -197,6 +197,90 @@ const tests: Array<ValidTestCaseSet<OptionsOf<typeof rule>>> = [
   },
   {
     code: dedent`
+      function y(x: {a: 1, b: object}) {
+        x.foo = "bar";
+        x["foo"] = "bar";
+        x.a += 1;
+        x.a -= 1;
+        x.a *= 1;
+        x.a /= 1;
+        x.a %= 1;
+        x.a <<= 1;
+        x.a >>= 1;
+        x.a >>>= 1;
+        x.a &= 1;
+        x.a |= 1;
+        x.a ^= 1;
+        x.a **= 1;
+        delete x.a;
+        delete x["a"];
+        x.a++;
+        x.a--;
+        ++x.a;
+        --x.a;
+        if (x.a = 2) {}
+        if (x.a++) {}
+        Object.assign(x, { c: "world" });
+        Object.assign(x.b, { c: "world" });
+        Object.defineProperties(x, { d: { value: 2, writable: false }});
+        Object.defineProperty(x, "e", { value: 3, writable: false });
+        Object.setPrototypeOf(x, null);
+      }
+    `,
+    optionsSet: [
+      [{ ignoreNonConstDeclarations: true }],
+      [
+        {
+          ignoreNonConstDeclarations: {
+            treatParametersAsConst: false,
+          },
+        },
+      ],
+    ],
+  },
+  {
+    code: dedent`
+      function y(mutable_x: {a: 1, b: object}) {
+        mutable_x.foo = "bar";
+        mutable_x["foo"] = "bar";
+        mutable_x.a += 1;
+        mutable_x.a -= 1;
+        mutable_x.a *= 1;
+        mutable_x.a /= 1;
+        mutable_x.a %= 1;
+        mutable_x.a <<= 1;
+        mutable_x.a >>= 1;
+        mutable_x.a >>>= 1;
+        mutable_x.a &= 1;
+        mutable_x.a |= 1;
+        mutable_x.a ^= 1;
+        mutable_x.a **= 1;
+        delete mutable_x.a;
+        delete mutable_x["a"];
+        mutable_x.a++;
+        mutable_x.a--;
+        ++mutable_x.a;
+        --mutable_x.a;
+        if (mutable_x.a = 2) {}
+        if (mutable_x.a++) {}
+        Object.assign(mutable_x, { c: "world" });
+        Object.assign(mutable_x.b, { c: "world" });
+        Object.defineProperties(mutable_x, { d: { value: 2, writable: false }});
+        Object.defineProperty(mutable_x, "e", { value: 3, writable: false });
+        Object.setPrototypeOf(mutable_x, null);
+      }
+    `,
+    optionsSet: [
+      [
+        {
+          ignoreNonConstDeclarations: { treatParametersAsConst: true },
+          ignoreAccessorPattern: "mutable*",
+        },
+      ],
+    ],
+  },
+  {
+    code: dedent`
       (mutable_foo as Bar).baz = "hello world";
     `,
     optionsSet: [[{ ignoreAccessorPattern: "mutable*.*" }]],
