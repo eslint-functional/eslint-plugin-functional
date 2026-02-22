3361016f925cea7e9570607577abd7fa96b858a2
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2026-02-23 00:23:16 +1300

    chore: remove vitest config changes (2)

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     exclude: ["lib", "node_modules"],
     testTimeout: 10_000,
     coverage: {
+      all: true,
       include: ["src"],
       exclude: ["lib"],
       reporter: ["lcov", "text"],
